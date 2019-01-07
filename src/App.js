3c22b32c7b5a6dc4358eaaec7861b048ec8e52e0
danchamorro <dan@danchamorro.com>
2019-01-07 12:05:01 -0500

    Added new books

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ class App extends Component {
       imageUrl:
         "https://upload.wikimedia.org/wikipedia/commons/e/ee/Mark_Twain_photo_portrait%2C_Feb_7%2C_1871%2C_cropped_Repair.jpg",
       imageSource: "Wikimedia Commons",
-      books: ["The Adventures of Huckleberry Finn"]
+      books: [
+        "The Adventures of Huckleberry Finn",
+        "Life on the Mississippi",
+        "Roughing It"
+      ]
     }
   };
 
