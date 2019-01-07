8f5157d0eb461aea149f0efed32f4055ad6b813f
danchamorro <dan@danchamorro.com>
2019-01-07 11:40:27 -0500

    Added some styles to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import Turn from "./components/Turn";
+import "./App.css";
 
 class App extends Component {
   state = {
     author: {
       name: "Mark Twain",
-      imageUrl: "images/authors/marktwain.jpg",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/e/ee/Mark_Twain_photo_portrait%2C_Feb_7%2C_1871%2C_cropped_Repair.jpg",
       imageSource: "Wikimedia Commons",
       books: ["The Adventures of Huckleberry Finn"]
     }
