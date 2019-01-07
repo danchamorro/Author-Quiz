ae1e8d2fe13bc13933b14f50489224bbfa06b48a
danchamorro <dan@danchamorro.com>
2019-01-07 00:45:51 -0500

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
         <Hero />
+        <Footer />
       </div>
     );
   }
